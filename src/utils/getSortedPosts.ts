b75a7b068c5b63de87420b3d10460b83597aa205
Mazen Alotaibi <mazen.ota@gmail.com>
2026-04-13 13:28:14 +0300

    Refactor getSortedPosts function to simplify date comparison by directly using pubDatetime, improving code readability and maintainability.

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -4,15 +4,7 @@ import { postFilter } from "./postFilter";
 export function getSortedPosts(posts: CollectionEntry<"posts">[]) {
   return posts.filter(postFilter).sort(
     (a, b) =>
-      Math.floor(
-        new Date(
-          b.data.modDatetime ?? b.data.pubDatetime
-        ).getTime() / 1000
-      ) -
-      Math.floor(
-        new Date(
-          a.data.modDatetime ?? a.data.pubDatetime
-        ).getTime() / 1000
-      )
+      Math.floor(new Date(b.data.pubDatetime).getTime() / 1000) -
+      Math.floor(new Date(a.data.pubDatetime).getTime() / 1000)
   );
 }
